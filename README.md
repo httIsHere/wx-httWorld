026d6b1138d337f32a325ec10ccefc74cde53eb5
httishere <18457730959@163.com>
2018-09-04 13:49:34 +0800

    add timer.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # wx-httWorld
 
 - ui: iview-webapp
+
+- 尝试一个时钟的效果，参考：https://github.com/kraaas/timer
---
+++ pages/timer/timer.js
@@ -0,0 +1,10 @@
+// pages/timer/timer.js
+Page({
+
+  data: {
+    leftDeg: 45,
+    rightDeg: -45,
+    remainTime: '05:00',
+    missionName: ''
+  }
+})
